c295cbdae286ffc72154d2caf7830ad58c13e2e9
k1921743 <sebi.mesca12@gmail.com>
2021-03-20 13:44:57 +0200

    id added

--- backend/src/main/java/com/mothercoders/mothercoders/Mayor.java
+++ backend/src/main/java/com/mothercoders/mothercoders/Mayor.java
@@ -1,8 +1,8 @@
 package com.mothercoders.mothercoders;
 
 public class Mayor extends com.mothercoders.mothercoders.User {
-    public Mayor(String lastName, String firstName, String birth, String mail, String phone) {
-        super(lastName, firstName, birth, mail, phone);
+    public Mayor(String lastName, String firstName, String birth, String mail, String phone, String idCity) {
+        super(lastName, firstName, birth, mail, phone, idCity);
     }
 
     @Override
