bf3b07daf7457c9ff0f975644cdad8f13e3449db
Edward <nechitoaiageorgeedward@gmail.com>
2021-03-20 23:42:53 +0200

    models

--- backend_node/models/userModel.mjs
+++ backend_node/models/userModel.mjs
@@ -1,7 +1,12 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    email: {type: String, required: true, unique: true},
+    cityName: {type: String, required: true, unique: true},
+    lastName: {type: String, required: true, unique: true},
+    firstName: {type: String, required: true, unique: true},
+    phone: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true, dropDups: true},
+    projectsVoted: {type: Array, required: true, unique: true},
     isAdmin: {type: Boolean, required: true, default:0},
     confirmedEmail: {type: Boolean, default: false}
 }, {
